885630fd7799d0bbb689bf30284b2dbf17a1bcd4
Praveen Oruganti <praveenorugantitech@gmail.com>
2021-01-09 03:06:38 +0530

    All About React JS

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+import Piano from "./components/Piano";
+
+function App() {
+  return (
+    <>
+      <div className="app-container">
+        <Piano />
+      </div>
+    </>
+  );
+
+}
+
+export default App;
