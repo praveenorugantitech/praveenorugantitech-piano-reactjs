fddd0f1149b297bb04e6dda3dacd62fa485a00dc
Praveen Oruganti <praveenorugantitech@gmail.com>
2021-01-25 01:42:08 +0530

    Updates

---
+++ precache-manifest.a20b09be313d9503e6a0bf005cba032c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cab82e5e4b0eaaf32a7b8bac5361e836",
+    "url": "/praveenorugantitech-piano-reactjs/index.html"
+  },
+  {
+    "revision": "f265f56f688e4d9392f8",
+    "url": "/praveenorugantitech-piano-reactjs/static/css/main.72b0e9fc.chunk.css"
+  },
+  {
+    "revision": "f9a71784ec61a0f00e0f",
+    "url": "/praveenorugantitech-piano-reactjs/static/js/2.90c688a2.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/praveenorugantitech-piano-reactjs/static/js/2.90c688a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f265f56f688e4d9392f8",
+    "url": "/praveenorugantitech-piano-reactjs/static/js/main.8c9a6c00.chunk.js"
+  },
+  {
+    "revision": "f9dc98c4613f022af063",
+    "url": "/praveenorugantitech-piano-reactjs/static/js/runtime-main.e7a45882.js"
+  }
+]);
